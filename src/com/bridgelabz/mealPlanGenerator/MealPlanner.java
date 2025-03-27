392b345bc41193c4e61527daf4c398784e575a5c
Yagyata <yagyatasharmaa@gmail.com>
2025-03-27 22:15:16 +0530

    [Yagyata] ADD: using generic method to validate and generate meal plan

--- src/com/bridgelabz/mealPlanGenerator/MealPlanner.java
+++ src/com/bridgelabz/mealPlanGenerator/MealPlanner.java
@@ -1,8 +1,28 @@
 package com.bridgelabz.mealPlanGenerator;
+import java.util.List;
+import java.util.ArrayList;
 
-public class MealPlanner {
+class MealPlanner {
     public static <T extends MealPlan> Meal<T> createMealPlan(T meal) {
         System.out.println("Processing meal plan: " + meal.getMealType());
         return new Meal<>(meal);
+    }
+}
+
+class MealManager {
+    private List<Meal<? extends MealPlan>> meals = new ArrayList<>();
+
+    public void addMeal(Meal<? extends MealPlan> meal) {
+        meals.add(meal);
+    }
+
+    public void displayMeals() {
+        for (Meal<? extends MealPlan> meal : meals) {
+            meal.displayMeal();
+        }
+    }
+
+    public boolean validateMeal(MealPlan meal) {
+        return meal.getMealName() != null && !meal.getMealName().isEmpty();
     }
 }
