42e770dfeb7dc858f5f94f1c7195b41c00ecfda3
Yagyata <yagyatasharmaa@gmail.com>
2025-03-27 11:30:07 +0530

    [Yagyata] ADD: Flight Management System with bounded types

---
+++ src/com/bridgelabz/flightScheduling/FlightManagement.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.flightScheduling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FlightManagement<T extends Number> {
+    private List<Flight<T>> flights = new ArrayList<>();
+
+    public void addFlight(Flight<T> flight) {
+        flights.add(flight);
+    }
+
+    public void displayAllFlights() {
+        for(Flight<T> flight : flights) {
+            flight.displayFlightInfo();
+        }
+    }
+}
