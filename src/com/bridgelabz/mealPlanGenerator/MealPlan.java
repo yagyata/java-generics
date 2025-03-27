e003b5efb5b581101176c20da32597d5d8fd7f13
Yagyata <yagyatasharmaa@gmail.com>
2025-03-27 22:07:34 +0530

    [Yagyata] ADD: interface for meal plan along with its subtypes

--- src/com/bridgelabz/mealPlanGenerator/MealPlan.java
+++ src/com/bridgelabz/mealPlanGenerator/MealPlan.java
@@ -2,28 +2,69 @@ package com.bridgelabz.mealPlanGenerator;
 
 interface MealPlan {
     String getMealType();
+    String getMealName();
 }
 
 class VegetarianMeal implements MealPlan {
-    public String getMealType(){
+    private String mealName;
+
+    public VegetarianMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getMealType() {
         return "Vegetarian Meal";
+    }
+
+    public String getMealName() {
+        return mealName;
     }
 }
 
 class VeganMeal implements MealPlan {
+    private String mealName;
+
+    public VeganMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
     public String getMealType() {
         return "Vegan Meal";
+    }
+
+    public String getMealName() {
+        return mealName;
     }
 }
 
 class KetoMeal implements MealPlan {
+    private String mealName;
+
+    public KetoMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
     public String getMealType() {
         return "Keto Meal";
     }
+
+    public String getMealName() {
+        return mealName;
+    }
 }
 
-class HighProtein implements MealPlan {
+class HighProteinMeal implements MealPlan {
+    private String mealName;
+
+    public HighProteinMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
     public String getMealType() {
         return "High-Protein Meal";
+    }
+
+    public String getMealName() {
+        return mealName;
     }
 }
