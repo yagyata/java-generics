5651f95665ec42e605c6120ebc18c04bf5fabb62
Yagyata <yagyatasharmaa@gmail.com>
2025-03-27 11:56:16 +0530

    Warehouse Management System

---
+++ src/com/bridgelabz/warehouseManagement/WarehouseItem.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.warehouseManagement;
+
+abstract class WarehouseItem {
+    private String name;
+    private double price;
+
+    public WarehouseItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    class Electronics extends WarehouseItem {
+        public Electronics(String name, double price) {
+            super(name, price);
+        }
+    }
+
+    class Groceries extends WarehouseItem {
+        public Groceries(String name, double price) {
+            super(name, price);
+        }
+    }
+
+    class Furniture extends WarehouseItem {
+        public Furniture(String name, double price) {
+            super(name, price);
+        }
+    }
+}
--- src/com/bridgelabz/warehouseManagement/WareouseItem.java
+++
@@ -1,4 +0,0 @@
-package com.bridgelabz.warehouseManagement;
-
-public class WareouseItem {
-}
